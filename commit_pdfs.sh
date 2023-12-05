9e027550c783f0b8e6ed049c2c3375690ca97e2b
xodix <kodemode@wp.pl>
2023-12-05 15:06:54 +0100

    Added notes from 05-12 15:06

---
+++ commit_pdfs.sh
@@ -0,0 +1,25 @@
+cd raw_notes
+# processes put into background for maximum speed
+for file in *.xopp ; do xournalpp -p "../pdfs/${file%.xopp}.pdf" "${file}" &> /dev/null & done ;
+
+
+raws=$(ls -l . | wc -l)
+curr_pdfs=$(ls -l ../pdfs/ | wc -l)
+
+while [ $raws != $curr_pdfs ]
+do
+	sleep 1
+	curr_pdfs=$(ls -l ../pdfs/ | wc -l)
+done
+
+echo "Created PDFs"
+
+d=$(date "+%d-%m %H:%M")
+
+git add "."
+git commit -m "Added notes from $d"
+git push
+
+
+
+# Kurwa prolog?
